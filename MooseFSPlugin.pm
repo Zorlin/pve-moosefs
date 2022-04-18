18db240e0c842b6f442b0f3879046a3de5a0f35d
Benjamin Arntzen <zorlin@gmail.com>
2022-04-19 05:26:48 +0800

    Theoretically handle mount/unmount

--- MooseFSPlugin.pm
+++ MooseFSPlugin.pm
@@ -13,7 +13,30 @@ use base qw(PVE::Storage::Plugin);
 # MooseFS helper functions
 
 sub moosefs_is_mounted {
-    die "Nope"
+    my ($mountpoint, $mountdata) = @_;
+
+    $mountdata = PVE::ProcFSTools::parse_proc_mounts() if !$mountdata;
+
+    die "$mountdata";
+
+    return $mountpoint if grep {
+        $_->[2] eq 'fuse.moosefs' &&
+        $_->[0] =~ /^\S+:\Q$volume\E$/ &&
+        $_->[1] eq $mountpoint
+    } @$mountdata;
+    return undef;
+}
+
+sub moosefs_mount {
+    my ($mfsmaster, $mountpoint) = @_;
+
+    my $cmd = ['/usr/bin/mfsmount', $mountpoint];
+
+    if ($mfsmaster) {
+        my $cmd = ['/usr/bin/mfsmount', $mfsmaster, $mountpoint];
+    }
+
+    run_command($cmd, errmsg => "mount error");
 }
 
 # Configuration
@@ -44,7 +67,63 @@ sub options {
     path => { fixed => 1 },
     subdir => { optional => 1 },
     disable => { optional => 1 },
+    mfsmaster => { optional => 1 },
     };
+}
+
+# Storage implementation
+
+sub status {
+    my ($class, $storeid, $scfg, $cache) = @_;
+
+    $cache->{mountdata} = PVE::ProcFSTools::parse_proc_mounts()
+        if !$cache->{mountdata};
+
+    my $path = $scfg->{path};
+
+    return undef if !moosefs_is_mounted($path, $cache->{mountdata});
+
+    return $class->SUPER::status($storeid, $scfg, $cache);
+}
+
+
+sub activate_storage {
+    my ($class, $storeid, $scfg, $cache) = @_;
+
+    # Get MooseFS master definition from config, otherwise return mfsmaster
+    my $mfsmaster = $scfg->{mfsmaster} ? $scfg->{mfsmaster} : 'mfsmaster';
+
+    $cache->{mountdata} = PVE::ProcFSTools::parse_proc_mounts()
+        if !$cache->{mountdata};
+
+    my $path = $scfg->{path};
+
+    if (!moosefsfs_is_mounted($path, $cache->{mountdata})) {
+        
+        mkpath $path if !(defined($scfg->{mkdir}) && !$scfg->{mkdir});
+
+        die "unable to activate storage '$storeid' - " .
+            "directory '$path' does not exist\n" if ! -d $path;
+
+        moosefs_mount($mfsmaster, $path);
+    }
+
+    $class->SUPER::activate_storage($storeid, $scfg, $cache);
+}
+
+sub deactivate_storage {
+    my ($class, $storeid, $scfg, $cache) = @_;
+
+    $cache->{mountdata} = PVE::ProcFSTools::parse_proc_mounts()
+        if !$cache->{mountdata};
+
+    my $path = $scfg->{path};
+    my $volume = $scfg->{volume};
+
+    if (moosefs_is_mounted($path, $cache->{mountdata})) {
+        my $cmd = ['/bin/umount', $path];
+        run_command($cmd, errmsg => 'umount error');
+    }
 }
 
 1;
